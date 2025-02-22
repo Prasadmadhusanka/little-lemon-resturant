874d4b0e8cb74aebf68e279829b2d34ded2c249b
Prasad Madushanka Dauglas <prasad.dream13@gmail.com>
2025-02-22 10:13:54 +0100

    I added semantic html elements to Header.js, Nav.js, Footer.js and I added meta tags and OGP meta tags and I created assets folder and added logos for little lemon resturant web app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,67 @@
+import logo_footer from "../assets/Logo_footer.png";
 
 function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="footer-column">
+          <img
+            src={logo_footer}
+            alt="Little Lemon Restaurant Logo"
+            width="50"
+          />
+        </div>
+
+        <div className="footer-column">
+          <h3>Doormat Navigation</h3>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/menu">Menu</a>
+            </li>
+            <li>
+              <a href="/reservations">Reservations</a>
+            </li>
+            <li>
+              <a href="/order_online">Order Online</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-column">
+          <h3>Contacts</h3>
+          <ul>
+            <li>Address: 123 Lemon Street, Chicago</li>
+            <li>Phone: +1 555-555-5555</li>
+            <li>Email: contact@littlelemon.com</li>
+          </ul>
+        </div>
+
+        <div className="footer-column">
+          <h3>Social Media</h3>
+          <ul>
+            <li>
+              <a href="https://facebook.com/littlelemon">Facebook</a>
+            </li>
+            <li>
+              <a href="https://twitter.com/littlelemon">Twitter</a>
+            </li>
+            <li>
+              <a href="https://instagram.com/littlelemon">Instagram</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
+import logo from '../assets/Logo.svg'
+import Nav from '../components/Nav'
 
 function Header() {
-  return <header></header>;
+  return <header>
+    <img src={logo} alt="Little Lemon Resturant Logo" width={150} />
+    <Nav></Nav>
+  </header>;
 }
 
 export default Header;

2d14142b1733f344bb92c2201673f705fa2550c2
Prasad Madushanka Dauglas <prasad.dream13@gmail.com>
2025-03-07 10:40:53 +0100

    I added responsiveness for NavBar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,22 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <nav className="navbar">
-      <ul>
+      {/* Hamburger Icon for Mobile */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      {/* Navbar Links */}
+      <ul className={`nav-links ${isMenuOpen ? "open" : ""}`}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -14,7 +27,7 @@ function Nav() {
           <a href="/menu">Menu</a>
         </li>
         <li>
-        <Link to="/reservation">Reservations</Link>
+          <Link to="/reservation">Reservations</Link>
         </li>
         <li>
           <a href="/order_online">Order Online</a>
